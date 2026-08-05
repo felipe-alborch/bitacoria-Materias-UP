7f02e6b4300bac423c14186a22d0f088b1959ec4
Alborch <felipe.alborch@accusys.com.ar>
2026-08-05 12:15:05 -0300

    Commit inicial para crear la carpeta de programación 2

---
+++ Programación 2/Guia de ejs 1/Ej1.c
@@ -0,0 +1,13 @@
+/*
+Calcular el indice de masa corporal ingresando el peso de una persona en kilos y su
+estatura en metros. Este indice se obtiene realizando el cociente entre el peso y el
+cuadrado de la estatura
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+    
+    return 0;
+}
