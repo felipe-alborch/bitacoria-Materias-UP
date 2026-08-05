7d29b956c460bcfce306bfd9ca91bccac5629ad6
Alborch <felipe.alborch@accusys.com.ar>
2026-08-05 14:30:01 -0300

    Ejs 1 y 2

--- Programación 2/Guia de ejs 1/Ej1.c
+++ Programación 2/Guia de ejs 1/Ej1.c
@@ -7,7 +7,18 @@ cuadrado de la estatura
 #include <stdio.h>
 
 int main() {
+    float imc = 0;
+    float altura = 0;
+    float peso = 0;
+
+    printf("Ingrese su peso: ");
+    scanf("%f", &peso);
+
+    printf("\nIngrese su altura: ");
+    scanf("%f", &altura);
     
+    imc = peso / (altura * altura);
+    printf("\nSu IMC es: %f", imc);
     
     return 0;
 }
---
+++ Programación 2/Guia de ejs 1/Ej2.c
@@ -0,0 +1,42 @@
+/*
+Ingresar el día y la cotización del dólar durante un mes. Calcular y mostrar el día
+donde hubo la mayor cotización.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int diasDelMes = 0;
+
+    do {
+        printf("Ingrese cuantos dias tiene el mes en curso: ");
+        scanf("%d", &diasDelMes);
+    } while (diasDelMes < 1 || diasDelMes > 30);
+    
+    
+    float cotizaciones[diasDelMes];
+
+    printf("Ingrese la cotizacion del dia 1: ");
+    scanf("%d", &cotizaciones[0]);
+
+    float cotizacionMaxima = cotizaciones[0];
+    int diaCotizacionMaxima = 1;
+
+    for(int i = 1; i < diasDelMes; i++) {
+        printf("Ingrese la cotizacion del dolar del dia %d: ", i+1);
+        scanf("%f", &cotizaciones[i]);
+
+        if(cotizaciones[i] > cotizacionMaxima) {
+            cotizacionMaxima = cotizaciones[i];
+            diaCotizacionMaxima = i+1;
+        }
+    }
+    
+    for(int i = 0; i < diasDelMes; i++) {
+        printf("\nCotizacion del dia %d: %f", i+1, cotizaciones[i]);
+    }
+
+    printf("\nLa cotizacion maxima encontrada fue de %f, el dia %d", cotizacionMaxima, diaCotizacionMaxima);
+
+    return 0;
+}
