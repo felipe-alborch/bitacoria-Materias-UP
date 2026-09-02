e6f40c0f18d75b0b801e89357a9e239b1a1a26a3
FelipeAlborch <82057459+FelipeAlborch@users.noreply.github.com>
2026-09-02 19:51:58 -0300

    Clase punteros

---
+++ Programación 2/Clases/clase5.c
@@ -0,0 +1,37 @@
+/*
+Vamos a cargar y a mostrar un vector con punteros.
+Cargarlo y mostralo con aritmetica de punteros.
+*/
+
+#include <stdio.h>
+
+void cargarVector(int *vector, int cantidad);
+void mostrarVector(int *vector, int cantidad);
+
+int main()
+{
+    int vector[5];
+
+    printf("\nCARGANDO VECTOR...\n");
+    cargarVector(vector, 5);
+
+    printf("\nMOSTRANDO VECTOR...\n");
+    mostrarVector(vector, 5);
+
+    return 0;
+}
+
+void cargarVector(int *vector, int cantidad)
+{
+    for(int i = 0; i < cantidad; i++)
+    {
+        printf("Ingrese el elemento %d: ", i + 1);
+        scanf("%d", vector + i);
+    }
+}
+
+void mostrarVector(int *vector, int cantidad)
+{
+    for(int i = 0; i < cantidad; i++)
+        printf("Elemento [%d]: %d\n", i+1, *(vector + i));
+}
