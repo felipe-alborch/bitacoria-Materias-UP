e69507bc9459c874f3246001cb721f4cc2159632
Alborch <felipe.alborch@accusys.com.ar>
2026-09-15 16:49:50 -0300

    ejs listas

---
+++ Programación 2/Guías de ejercicios/Guia de ejs 7/Ej8.c
@@ -0,0 +1,150 @@
+/*
+Con una lista ingresada, hacer funciones para:
+ - Ordenar una lista
+ - Eliminar un numero (si aparece mas de una vez, eliminar todas las coincidencias)
+ - Buscar un elemento
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Nodo {
+    int numero;
+    struct Nodo *siguiente;
+} Nodo;
+
+void mostrarLista(Nodo *lista);
+void liberarLista(Nodo *lista);
+Nodo *ingresarDatos(Nodo *lista);
+Nodo *insertarNodo(Nodo *lista, int valor);
+Nodo *ordenarLista(Nodo *lista);
+void buscarElemento(Nodo *lista, int buscado);
+
+int main() {
+    Nodo *lista = NULL;
+    int elementoABuscar;
+
+    printf("\n\n--- INGRESANDO ELEMENTOS A LA LISTA ---\n\n");
+    lista = ingresarDatos(lista);
+
+    printf("\n\n--- MOSTRANDO ELEMENTOS DE LA LISTA ---\n\n");
+    mostrarLista(lista);
+
+    printf("\n\n--- ORDENANDO ELEMENTOS DE LA LISTA ---\n\n");
+    lista = ordenarLista(lista);
+
+    printf("\n\n--- MOSTRANDO ELEMENTOS DE LA LISTA ---\n\n");
+    mostrarLista(lista);
+
+    printf("\n\n--- BUSCANDO UN ELEMENTO DE LA LISTA ---\n\n");
+    printf("Ingrese un elemento para buscar en la lista: ");
+    scanf("%d", &elementoABuscar);
+    buscarElemento(lista, elementoABuscar);
+
+    printf("\n\n--- LIBERANDO LA LISTA ---\n\n");
+    liberarLista(lista);
+    
+    return 0;
+}
+
+Nodo *ingresarDatos(Nodo *lista) {
+    int numero;
+    
+    printf("Ingresa un numero: ");
+    scanf("%d", &numero);
+
+    while(numero != 0) {
+        lista = insertarNodo(lista, numero);
+        
+        printf("Ingresa un numero: ");
+        scanf("%d", &numero);
+    }
+
+    return lista;
+}
+
+Nodo *insertarNodo(Nodo *lista, int valor) {
+    Nodo *nuevo = malloc(sizeof(Nodo));
+
+    if (nuevo == NULL) {
+        printf("No se pudo reservar memoria.\n");
+        return lista;
+    }
+
+    nuevo->numero = valor;
+    nuevo->siguiente = NULL;
+
+    if (lista == NULL) {
+        return nuevo;
+    }
+
+    Nodo *actual = lista;
+
+    while (actual->siguiente != NULL) {
+        actual = actual->siguiente;
+    }
+
+    actual->siguiente = nuevo;
+
+    return lista;
+}
+
+void mostrarLista(Nodo *lista) {
+    Nodo *actual = lista;
+    int contador = 1;
+
+    while (actual != NULL) {
+        printf("Numero [%d]: %d\n", contador, actual->numero);
+        actual = actual->siguiente;
+
+        contador += 1;
+    }
+}
+
+void liberarLista(Nodo *lista) {
+    Nodo *actual = lista;
+
+    while (actual != NULL) {
+        Nodo *siguiente = actual->siguiente;
+
+        free(actual);
+
+        actual = siguiente;
+    }
+}
+
+Nodo *ordenarLista(Nodo *lista) {
+    Nodo *actual = lista, *siguiente = NULL;
+    int aux;
+
+    for (actual; actual != NULL; actual = actual->siguiente) {
+        for (siguiente = actual->siguiente; siguiente != NULL; siguiente = siguiente->siguiente) {
+            if (actual->numero > siguiente->numero) {
+                aux = actual->numero;
+                actual->numero = siguiente->numero;
+                siguiente->numero = aux;
+            }
+        }
+    }
+
+    return lista;
+}
+
+void buscarElemento(Nodo *lista, int buscado) {
+    Nodo *actual = lista;
+    int contador = 1, cantHits = 0;
+
+    while(actual != NULL) {
+        if(actual->numero == buscado) {
+            printf("Se encontro el numero [%d] en la posicion [%d].\n", actual->numero, contador);
+            cantHits += 1;
+        }
+        
+        contador += 1;
+        actual = actual->siguiente;
+    }
+
+    if(cantHits == 0)
+        printf("No se encontraron elementos.\n");
+}
